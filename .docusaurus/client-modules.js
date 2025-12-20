cae6e588e3f10b412ba000d29e4ab8817ed985d7
Utkarsh Verma <vermautkarsh901@gmail.com>
2025-12-20 19:07:10 +0530

    chore: remove Docusaurus build artifacts

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/home/uverma/bluethroat-docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/home/uverma/bluethroat-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/home/uverma/bluethroat-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/uverma/bluethroat-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/uverma/bluethroat-docs/src/css/custom.css"),
-];
